dbbbbdb66b561bdb3c728fc0d155ca0dad91ae1b
Kyle Nitzsche <kyle.nitzsche@canonical.com>
2020-09-29 14:29:19 -0400

    Remove some inherited stuff

--- scripts/bin/record_music.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Pull GPIO 21 High
-# gpio is already exported by gadget snap
-# echo 408 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio408/direction
-echo 1 > /sys/class/gpio/gpio408/value
-
-echo "^^^^^mic initialised^^^^^"
